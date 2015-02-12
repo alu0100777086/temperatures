65e9f95ffda301d367716fa56e800745e6460ed5
Marcos Luis <alu0100777086@ull.edu.es>
2015-02-12 09:24:01 +0000

    Cambios

---
+++ tests/tests.js
@@ -0,0 +1,19 @@
+var assert = chai.assert;
+
+suite('temperature', function() {
+    test('32F = 0C', function() {
+        original.value = "32F";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "0.00 C");
+    });
+    test('45C = 113.0 Farenheit', function() {
+        original.value = "45C";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "113.00 F");
+    });
+    test('5X = error', function() {
+        original.value = "5X";
+        calculate();
+        assert.match(converted.innerHTML, /ERROR/);
+    });
+});
